17c2973fb29f7873ec5dcc4dad782488a0406c46
Diego <diegocosillo@gmail.com>
2024-10-16 12:16:58 -0600

    Login

---
+++ CineGT/CineGT/Controllers/AccesoController.cs
@@ -0,0 +1,38 @@
+﻿using CineGT.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace CineGT.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Usuario oUsuario)
+        {
+            string user = oUsuario.User;
+            string clave = oUsuario.Clave;
+            string connectionString = $"Server=DIEGO\\SQLEXPRESS;Database=CineGT;User Id={user};Password={clave};";
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    cn.Open();
+                    HttpContext.Session.SetString("Usuario", user);
+                    return RedirectToAction("Index", "Home");
+                }
+            }catch (SqlException)
+            {
+                ViewData["Mensaje"] = "Usuario o contraseña incorrectos.";
+                return View();
+            }
+
+        }
+
+    }
+}
